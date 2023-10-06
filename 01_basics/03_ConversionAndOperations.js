a7cf64c249c7f0819ee66c0df3b42c41f512f9c1
nitindixit493 <nitindixit493@gmail.com>
2023-10-06 19:33:06 +0000

    Conversion

---
+++ 01_basics/03_ConversionAndOperations.js
@@ -0,0 +1,12 @@
+let score=false
+// console.log(typeof score)
+// console.log(typeof(score));
+
+let num=Number(score);
+// console.log(num);
+// console.log(typeof num);
+
+let isLogged=1
+console.log(typeof isLogged);
+let isBoolean =Boolean(1)
+console.log(isBoolean);
