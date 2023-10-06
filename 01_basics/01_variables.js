5d34aeeef00e3b34a91c57cc751bbb6f45c4b51b
nitindixit493 <nitindixit493@gmail.com>
2023-10-06 19:16:44 +0000

    02_DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const myName="Nitin";
+let lastname="Kumar"
+var email="n@Gmail.com"
+password="1234"
+let com;
+com="hee"
+console.log(myName);
+console.table([myName,lastname,email,password,com])
---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,15 @@
+"use Strict";
+
+console.log(3 + 3)
+console.log("Nitin")
+
+//number
+//bigInt
+//String
+//boolean
+//null
+//undefined
+//symbol => unique
+
+//object
+console.log(typeof null);
